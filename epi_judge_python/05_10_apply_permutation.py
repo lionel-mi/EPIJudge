50dba76ed0b23ee4748b219474e98bd544fd9f55
lionelm <lionelm@wix.com>
2024-03-13 08:12:25 +0200

    sol 5.10

--- epi_judge_python/05_10_apply_permutation.py
+++ epi_judge_python/05_10_apply_permutation.py
@@ -8,13 +8,13 @@ def apply_permutation(perm: List[int], A: List[int]) -> None:
     for next_head in range(len(A)):
         if is_permutated[next_head]:
             continue
+        is_permutated[next_head] = True
         prev_idx = None
         next_idx = perm[next_head]
-        tmp = A[next_idx]
+        tmp = A[next_head]
         while prev_idx != next_head:
-            if prev_idx is None:
-                prev_idx = next_head
-            A[next_idx] = A[prev_idx]
+            A[next_idx], tmp = tmp, A[next_idx]
+            is_permutated[next_idx] = True
             prev_idx = next_idx
             next_idx = perm[next_idx]
 
@@ -26,6 +26,8 @@ def apply_permutation_wrapper(perm, A):
     return A
 
 
+
+apply_permutation([2, 0, 1, 3], ['a', 'b', 'c', 'd'])
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('05_10_apply_permutation.py',
